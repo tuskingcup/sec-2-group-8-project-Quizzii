2c9e94f7b26e0104b2e99d53da68918506c5b022
Panupong <panupong.n@mail.kmutt.ac.th>
2022-03-28 15:46:27 +0700

    list cartoon 1

---
+++ src/components/CartoonList.vue
@@ -0,0 +1,38 @@
+<script setup>
+const props = defineProps({
+  cartoonList: {
+    type: Array,
+    require: true,
+  },
+  currentHover: {
+    type: String,
+    require: true,
+  },
+  checkHover: {
+    type: Boolean,
+    require: true,
+  },
+})
+</script>
+
+<template>
+  <div class="flex flex-wrap justify-center mt-5">
+    <div
+      id="listcartoons"
+      :class="'card-box w-72 h-96 rounded overflow-hidden shadow-lg m-5 transform transition duration-500 hover:scale-110'"
+      :style="[currentHover === key ? {'background-image' : '', 'background-size': 'cover'} : {'background-image': 'url(' + cartoon.img + ')', 'background-size': 'cover'}]"
+      v-for="(cartoon, key) in cartoonList"
+      @mouseover="currentHover = key, checkHover = true"
+      @mouseleave="currentHover = null, checkHover = false"
+    >
+    <div class="m-2" v-show="currentHover === key ">
+        <p class="text-lg font-semibold mt-3 px-2 flex-1 ">{{cartoon.name}}</p>
+        <p class="px-2 text-sm">{{cartoon.writer}}</p>
+        <p class="my-3 px-2">{{cartoon.synopsis}}</p>
+    </div>
+      
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,34 @@
 <script setup>
-    import {onBeforeMount} from 'vue'
+    import { ref, onBeforeMount} from 'vue'
+import CartoonList from '../components/CartoonList.vue';
+const cartoonList = ref([])
 
+const httpCartoon = 'http://localhost:5000/cartoons'
+const currentHover = ref('')
+const checkHover = ref(false)
     
+const getCartoonList = async () => {
+    const resCartoon = await fetch(httpCartoon)
+    if(resCartoon.status === 200){
+        cartoonList.value = await resCartoon.json()
+        console.log(cartoonList.value)
+    }else{
+        console.log(`error, cannot get cartoons`)
+    }
+}
+
+onBeforeMount(async () => {
+  await getCartoonList() //ต้อง async await เหมือนกัน
+})
+
 </script>
  
 <template>
     <div>
-        <h1>HOME</h1>
+        <h1>Cartoon List</h1>
+    </div>
+    <div>
+        <CartoonList :cartoonList="cartoonList" :checkHover="checkHover" :currentHover="currentHover"></CartoonList>
     </div>
 </template>
  
